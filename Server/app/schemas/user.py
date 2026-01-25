e89de3a003e5b6b968a12af9cbb243ae88861f9d
Lord-Selasi <selasi209@gmail.com>
2026-01-25 09:42:42 -0500

    user schema implemented

--- Server/app/schemas/user.py
+++ Server/app/schemas/user.py
@@ -6,7 +6,7 @@ from typing import Optional
 class UserCreate(BaseModel):
     email: EmailStr  
     username: str = Field(..., min_length=3, max_length=50)
-    password: str = Field(..., min_length=8, max_length=100)
+    password: str = Field(..., min_length=8, max_length=72)
 
 
 # Login schema
