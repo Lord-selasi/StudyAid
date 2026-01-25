9c186bef751d879ed57c9ae99d8bcb30384dfb58
Lord-Selasi <selasi209@gmail.com>
2026-01-25 09:38:23 -0500

    fix: hash algorithm updated

--- Server/app/config.py
+++ Server/app/config.py
@@ -7,9 +7,11 @@ class Settings(BaseSettings):
     APP_NAME: str = "Study Aid API"
     DEBUG: bool = False
     API_V1_PREFIX: str = "/api/v1"
-    
+
     # Security
     SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
     # Database
     DATABASE_URL: str
